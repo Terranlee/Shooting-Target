746056b4ff17e774d321b4f069d79e631fbf14cc
Terranlee <ltrthu@163.com>
2016-03-15 22:34:44 +0800

    update code: add color picking

--- shooting.cpp
+++ shooting.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -19,15 +20,68 @@ bool loadAndScale(const string& filename, Mat& pic){
     return true;
 }
 
+void onMouseCatchColor(int mouseEvent, int x, int y, int flags, void* param){
+    switch(mouseEvent){
+        case CV_EVENT_LBUTTONDOWN:{
+            Point* p = (Point*)param;
+            cout<<"catch color at: ("<<y<<", "<<x<<")    "<<endl;
+            *p = Point(x, y);
+            break;
+        }
+        default:{}
+    }
+}
+
+void RGB2HSVEqualize(const Mat& picRGB, Mat& picHSV){    
+    cout<<"a"<<endl;
+    cvtColor(picRGB, picHSV, COLOR_BGR2HSV);
+    cout<<"a"<<endl;
+    // equalize
+    vector<Mat> hsvSplit;
+    split(picHSV, hsvSplit);
+    equalizeHist(hsvSplit[2],hsvSplit[2]);
+    merge(hsvSplit,picHSV);
+}
+
+void catchColor(const Mat& pic){
+    namedWindow("background");
+    imshow("background", pic);
+    Point p_back, p_tar;
+    setMouseCallback("background", onMouseCatchColor, &p_back);
+    waitKey(0);
+    //destroyWindow("background");
+
+/*
+    namedWindow("catch target");
+    imshow("catch target", pic);
+    setMouseCallback("catch target", onMouseCatchColor, &p_tar);
+    waitKey(0);
+    destroyWindow("catch target");
+*/
+    
+    Mat picHSV;    
+    RGB2HSVEqualize(pic, picHSV);
+
+    Vec3b background = picHSV.at<Vec3b>(p_back);
+
+    Vec3b low = background - Vec3b(50, 50, 50);
+    Vec3b high = background + Vec3b(50, 50, 50);
+
+    Mat picThreshold;
+    inRange(picHSV, low, high, picThreshold);
+    
+    namedWindow("answer");
+    imshow("answer", picThreshold);
+    waitKey(0);
+    destroyWindow("answer");
+}
+
 int main(int argc, char const *argv[])
 {
     string filename = "origin/image1.JPG";
     Mat pic;
     loadAndScale(filename, pic);
-
-    namedWindow("cv");
-    imshow("cv", pic);
-    waitKey(0);
+    catchColor(pic);
 
     return 0;
 }
